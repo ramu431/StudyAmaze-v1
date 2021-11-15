05f6ce5ffbed7c3a4ee1d6de2cb71f15a345b610
Sairebba1544 <sairebbaanalogit@gmail.com>
2021-11-15 19:01:04 +0530

    Changes made

---
+++ src/app/student/time-table/time-table.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { StudentService } from 'src/app/services/student.service';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-time-table',
+  templateUrl: './time-table.component.html',
+  styleUrls: ['./time-table.component.css']
+})
+export class TimeTableComponent implements OnInit {
+
+  weekdays : any;timeslots : any;timetable : any;
+
+  constructor(private _location:Location, private student:StudentService) { }
+
+  ngOnInit(): void {
+    let section = "A";
+    if(sessionStorage.getItem('section')){
+      section = sessionStorage.getItem('section');
+    }
+    this.student.getTimeTable(sessionStorage.getItem('instituteId'),sessionStorage.getItem('standard'),section).subscribe(
+      (data:any)=>{
+        console.log(data);
+        this.weekdays = data[0].weekDays;
+        this.timeslots = data[0].timeSlotsDTO;
+        this.timetable = data[0].timeTableInfoDTO[0];
+      }
+    );
+  }
+
+  goBack(){
+    this._location.back();
+  }
+
+}
